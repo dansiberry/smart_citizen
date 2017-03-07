3c3e9aea4b883b1622c7ab84b11f35e06e4f6d06
Dan Siberry <dansiberry1@gmail.com>
2017-03-07 10:22:18 +0100

    Added controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,40 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+
+  get 'comments/create'
+
+  get 'comments/edit'
+
+  get 'comments/update'
+
+  get 'comments/destroy'
+
+  get 'posts/update'
+
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/edit'
+
+  get 'posts/show'
+
+  get 'posts/index'
+
+  get 'posts/destroy'
+
+  get 'post/new'
+
+  get 'post/create'
+
+  get 'post/edit'
+
+  get 'post/show'
+
+  get 'post/index'
+
+  get 'post/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
