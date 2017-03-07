792f3c22dca6546b40245d73e5a6c3cbc4032bb0
andreasdieryck <andreas.dieryck@gmail.com>
2017-03-07 14:36:16 +0100

    Homepage and navbar done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    lastrecord = Post.last
     @postrand = []
     3.times do
-      post = Post.rand(1..lastrecord)
-      @postsrand << post
+      post = Post.find(1)
+      @postrand << post
     end
+    @postrand
   end
 
 end
