0d3f92c2ecfc49ecb60e0cdfbc792ad3c8d61d3a
Erin Douglas <erin.douglas@riseup.net>
2017-03-07 17:01:17 +0100

    Category filter works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,12 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @list_categories = Post.all_categories
+    if params[:category].present?
+      @posts = Post.by_category(params[:category])
+    else
+      @posts = Post.all
+    end
   end
 
   def destroy
@@ -30,6 +35,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-      params.require(:post).permit(:title, :content, :category, :city, :neighbourhood,:photo, :photo_cache,:photo2, :photo2_cache,:photo3, :photo3_cache)
+    params.require(:post).permit(:title, :content, :category, :city, :neighbourhood,:photo, :photo_cache,:photo2, :photo2_cache,:photo3, :photo3_cache)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_comments
   has_many :comments, through: :post_comments
+
+  scope :by_category, -> (category) { where(category: "#{category}") }
+
+  def self.all_categories
+    self.all.map { |post| post.category }.uniq.sort
+  end
 end
