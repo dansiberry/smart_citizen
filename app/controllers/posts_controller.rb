b85b7f3bc99b8843ce88aca748a8c3bd9d8f4fb8
Dan Siberry <dansiberry1@gmail.com>
2017-03-07 14:32:28 +0100

    Added the functions for the comments and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,20 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.new(post_params)
+    post.user = current_user
+    post.save
+    redirect_to post_path(post)
   end
 
   def edit
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def index
@@ -18,5 +24,11 @@ class PostsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def post_params
+      params.require(:post).permit(:title, :content, :category, :city, :neighbourhood,:photo, :photo_cache,:photo2, :photo2_cache,:photo3, :photo3_cache)
   end
 end
