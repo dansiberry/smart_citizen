fe4dda8d546aaea9b3eff66e6f36451b6c6ec3e9
Dan Siberry <dansiberry1@gmail.com>
2017-03-07 15:59:39 +0100

    Created Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.new(email: "test@email.com", password: "password")
-user.save
-comment = Comment.new(content: "This is the comment content!")
-comment.save
-post = Post.new(title: "This is the post title")
-post.save
+10.times do
+    user = User.new(email: Faker::Internet.email, password: "password", city: "Barcelona", address: ["Gracia", "Sants", "Poble Sec", "Poble Nou"].sample(1)[0], first_name: Faker::Name.first_name, last_name: Faker::Name.last_name )
+    user.save
+    comment = Comment.new(content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam sint suscipit facilis, veniam adipisci sunt, ut doloremque qui ipsa commodi dolorem fugiat iure? Quidem, odit velit recusandae. Quia dolor veritatis modi nam non voluptatum, at blanditiis quas atque nisi, qui.")
+    comment.save
+    post = Post.new(title: Faker::Commerce, content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis deleniti, itaque et enim rem ex iure suscipit molestias quia fugiat. Nesciunt reiciendis quis quam eligendi omnis eum harum et quo consequatur impedit tempora quae vitae magni necessitatibus blanditiis doloribus, iste earum quasi quibusdam aspernatur! Quos quidem iure sint, sunt harum.", category: "issue-category"  ,city: "Barcelona", neighbourhood: ["Gracia", "Sants", "Poble Sec", "Poble Nou"].sample(1)[0] )
+    post.save
 
-user.posts << post
-user.comments << comment
-post.comments << comment
-
+    user.posts << post
+    user.comments << comment
+    post.comments << comment
+end
