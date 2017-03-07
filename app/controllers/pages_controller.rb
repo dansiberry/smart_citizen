2e60a659d19a31dce353c9b9eff3f13b31fd3823
andreasdieryck <andreas.dieryck@gmail.com>
2017-03-07 14:23:00 +0100

    homepage random generator completed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    lastrecord = Post.last
+    @postrand = []
+    3.times do
+      post = Post.rand(1..lastrecord)
+      @postsrand << post
+    end
   end
 
 end
