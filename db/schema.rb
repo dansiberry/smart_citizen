ba10a7430e495344f0de01d37429300d075c1ecd
andreasdieryck <andreas.dieryck@gmail.com>
2017-03-08 15:56:35 +0100

    small changes in preparation for photos rollout

--- db/schema.rb
+++ db/schema.rb
@@ -84,13 +84,12 @@ ActiveRecord::Schema.define(version: 20170308110005) do
     t.string   "last_name"
     t.string   "address"
     t.string   "city"
-    t.string   "postal_code"
+    t.string   "neighbourhood"
     t.string   "provider"
     t.string   "uid"
     t.string   "twitter_picture_url"
     t.string   "token"
     t.datetime "token_expiry"
-    t.string   "neighbourhood"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
