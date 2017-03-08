7232f61ad995b6c0e204b8264abc2a4bcd0ea2e4
Erin Douglas <erin.douglas@riseup.net>
2017-03-08 18:59:46 +0100

    Pundit has been implemented

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -4,11 +4,6 @@ class PoliticiansController < ApplicationController
     authorize @user
   end
 
-  def destroy
-    UserAsPolitician.find(current_user.as_politician.id).destroy
-    redirect_to root_path
-  end
-
   def edit
     @user = current_user
     @political_info = @user.as_politician || UserAsPolitician.new(user_id: current_user.id)
@@ -21,17 +16,24 @@ class PoliticiansController < ApplicationController
     @user = current_user
     authorize @user
     @political_info = UserAsPolitician.new(user_as_politician_params)
-    authorize @political_info
     @political_info.user = current_user
+    authorize @political_info
     @political_info.save
     @user.save
     redirect_to politician_path(@political_info)
   end
 
+  def destroy
+    political_info = UserAsPolitician.find(current_user.as_politician.id)
+    authorize political_info
+    political_info.destroy
+    redirect_to root_path
+  end
+
   private
 
   def user_as_politician_params
-      params.require(:user_as_politician).permit(:elected, :elected, :political_party, :bio, :responsabilities, :photo, :photo_cache)
+    params.require(:user_as_politician).permit(:elected, :elected, :political_party, :bio, :responsabilities, :photo, :photo_cache)
   end
 
 end
