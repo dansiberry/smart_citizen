41d809e3771b5c3ae6c5a3ce696e52b3aa2512f9
Dan Siberry <dansiberry1@gmail.com>
2017-03-09 17:50:38 +0100

    beginning of votable

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   has_many :comments, through: :post_comments
   has_many :users, through: :user_posts
   has_many :user_posts
+  acts_as_votable
 
   validates :title, presence: true
   validates :category, presence: true
@@ -14,6 +15,18 @@ class Post < ApplicationRecord
 
   scope :by_category, -> (category) { where(category: category) }
   scope :by_neighbourhood, -> (neighbourhood) { where(neighbourhood: neighbourhood)}
+
+  def upvote
+    @post = Post.find(params[:id])
+    @post.upvote_by current_user
+    redirect_to :back
+  end
+
+  def downvote
+    @post = Post.find(params[:id])
+    @post.downvote_by current_user
+    redirect_to :back
+  end
 
   def self.all_categories
     self.all.map { |post| post.category }.select { |a| a.present? }.uniq.sort
