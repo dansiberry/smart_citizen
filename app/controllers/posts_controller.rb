e42af0221f561d27782b7b934a29f079522b46d9
Erin Douglas <erin.douglas@riseup.net>
2017-03-09 12:28:28 +0100

    Related posts appear on the posts#show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,11 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+
+    # this is for the related posts
     @related_posts = Post
-    @related_posts = @related_posts.by_category(@post.category)
+    @related_posts = @related_posts.by_category(@post.category).to_a
+    @related_posts = @related_posts.delete_if { |related_post| related_post.id == @post.id }
   end
 
   def index
