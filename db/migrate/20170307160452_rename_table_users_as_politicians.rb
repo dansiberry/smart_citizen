58dedb5c1706e4b53552586590900fa06dc1a990
Dan Siberry <dansiberry1@gmail.com>
2017-03-09 15:52:27 +0100

    fixing problems really really problems

--- db/migrate/20170307160452_rename_table_users_as_politicians.rb
+++ db/migrate/20170307160452_rename_table_users_as_politicians.rb
@@ -2,7 +2,7 @@ class RenameTableUsersAsPoliticians < ActiveRecord::Migration[5.0]
 
 
   def change
-    rename_table :user_as_politicians, :user_as_politicians
+    rename_table :user_as_politicans, :user_as_politicians
   end
 
 
