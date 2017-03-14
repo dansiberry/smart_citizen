7f5b1923a641634f1793ca3eecd194e5d47f0f8c
Erin Douglas <erin.douglas@riseup.net>
2017-03-14 17:32:47 +0100

    Posts#show page is styled

---
+++ app/assets/javascripts/issues.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".toggle").click(function() {
+    var $currentToggle = $(this);
+    var blockId = $currentToggle.attr("id");
+    var targetContentId = blockId + "-content";
+
+    $(".issues-replies-text").hide();
+    $("#" + targetContentId).show(); });
+
+  $(".toggle").first().click();
+});
