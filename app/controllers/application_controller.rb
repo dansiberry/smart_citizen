26e045b2149fa5230b8fb6209b58a94e48b4c4df
Erin Douglas <erin.douglas@riseup.net>
2017-03-16 10:54:27 +0100

    When a user signs in or signs up, they are redirected to their previous page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,15 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  after_filter :store_location
+
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+
   def after_sign_in_path_for(resource)
-    posts_path
+    session[:previous_url] || root_path
   end
 
   private
