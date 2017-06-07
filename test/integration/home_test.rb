51b28fffaed37047471a1a27a9d48bc2634930f8
Dan Siberry <dansiberry1@gmail.com>
2017-06-07 09:23:25 +0200

    travis

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -4,5 +4,6 @@ class HomeTest < ActionDispatch::IntegrationTest
   test "Homepageloads correctly" do
     visit "/"
     assert_equal 200, page.status_code
+    assert_equal 5 , User.all.length
   end
 end
