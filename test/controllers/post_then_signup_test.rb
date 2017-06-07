c95146c3113686d8be410e483f50cce0b794820e
Dan Siberry <dansiberry1@gmail.com>
2017-06-07 12:56:54 +0200

    travis

---
+++ test/controllers/post_then_signup_test.rb
@@ -0,0 +1,22 @@
+# require 'test_helper'
+
+# class HomeTest < ActionDispatch::IntegrationTest
+#   test "User can access post/new wihtout logging in" do
+#     visit "./post/new"
+#     assert_equal 200, page.status_code
+#   end
+
+  # test "If user is not logged in, signup form is nested in post form" do
+  #   visit "./"
+  #   assert_equal 200, page.status_code
+  #   assert_equal 5 , User.all.length
+  # end
+
+  # test "User can create post and signup at once" do
+  #   visit "./"
+  #   assert_equal 200, page.status_code
+  #   assert_equal 5 , User.all.length
+  # end
+# end
+
+
