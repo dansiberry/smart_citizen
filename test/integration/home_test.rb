9e96a9e05705719e6488b052627bdf5b059a3a8f
Dan Siberry <dansiberry1@gmail.com>
2017-06-07 11:28:12 +0200

    travis...

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class HomeTest < ActionDispatch::IntegrationTest
   test "Homepageloads correctly" do
-    # visit "/"
-    # assert_equal 200, page.status_code
+    visit "./"
+    assert_equal 200, page.status_code
     assert_equal 5 , User.all.length
   end
 end
