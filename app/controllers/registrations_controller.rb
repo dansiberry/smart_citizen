179f13b5bf1c228d5de7011b1ef599a4e7e7e9c4
Erin Douglas <erin.douglas@riseup.net>
2017-08-31 15:31:00 +0200

    Fixed post-signup redirect error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,24 +9,4 @@ class RegistrationsController < Devise::RegistrationsController
     end
   end
 
- def create
-  super
-  if resource.active_for_authentication?
-    flash[:analytics] = '/goals/signup'
-    set_flash_message! :notice, :signed_up
-    sign_up(resource_name, resource)
-    respond_with resource, location: after_sign_up_path_for(resource)
-  else
-    super
-  end
-  super
-end
-
-def destroy
-  resource.destroy
-  Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-  flash[:analytics] = '/goals/delete_account'
-  super
-end
-
 end
