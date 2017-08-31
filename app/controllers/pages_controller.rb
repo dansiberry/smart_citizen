cecbcf7c82aa4818d02a553c88af58b7b8e1a737
Erin Douglas <erin.douglas@riseup.net>
2017-08-31 15:57:11 +0200

    Stopped unverified issues from appearing in the Most Popular section on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :about_us ]
 
    def home
-    @posts = Post.all
+    @posts = Post.verified_posts
     @upvoted_posts = @posts.sort_by {|a| a.votes_for.size}
     @upvoted_posts = @upvoted_posts.reverse
     @upvoted_posts = @upvoted_posts.first(3)
